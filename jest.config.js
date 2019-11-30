914985047b4a8d07dae9cf45a074c5be4fc0ed7c
FPierre <pflauder@gmail.com>
2019-11-30 17:09:25 +0100

    Improve tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  globalSetup: './tests/setup.js',
-  setupFilesAfterEnv: ['./tests/setup-after-env.js'],
   coverageDirectory: './coverage',
   reporters: ['default', 'jest-junit'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
 };
