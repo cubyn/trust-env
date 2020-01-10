7c27dc5f1682d1d2ed454c870a93906712af21f4
FPierre <pflauder@gmail.com>
2020-01-10 16:53:13 +0100

    Check variables depends on a given type

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-const { ProcessEnvError } = require('./errors');
+const isJs = require('is_js');
 
-module.exports = (variables) => {
-  Object.entries(variables).forEach(([key, value]) => {
-    if (!value) {
-      throw new ProcessEnvError(key);
+// TODO not possible to have both default and required options
+// const CONTRACT = [{
+//   variable: 'DB_HOST',
+//   type: 'string',
+//   // required: true,
+//   // default: '',
+//   // Called at runtime only
+//   // Option to call it each time
+//   // validate: () => {},
+//   // Call each time
+//   // transform: () => {},
+// }];
+
+const validateType = declaration => isJs[declaration.type](process.env[declaration.variable]);
+
+const validate = (contract) => {
+  const validations = contract.map((declaration) => {
+    if (declaration.type) {
+      return validateType(declaration);
     }
+
+    if (declaration.validate) {
+      return declaration.validate(declaration);
+    }
+
+    // TODO Custom error
+    throw new Error();
   });
+
+  const allValid = validations.every(validation => validation === true);
+
+  if (allValid) {
+    return allValid;
+  }
+
+  throw new Error(validations);
+};
+
+// module.exports = (contract, { logger = console } = { logger: console }) => {
+module.exports = {
+  validate,
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,25 +1,73 @@
-const { ProcessEnvError } = require('./errors');
-const envValidation = require('.');
+// const { ProcessEnvError } = require('./errors');
+const env = require('.');
 
 describe('src/index.js', () => {
-  describe('when there is env variable missing value', () => {
-    it('throws with ProcessEnvError', () => {
-      expect(() => envValidation({ AMQP_HOST: null }))
-        .toThrow(ProcessEnvError);
+  beforeEach(() => {
+    process.env.DB_HOST = 'localhost';
+  });
+
+  describe('when the variable is not found', () => {
+    describe('when the default is given', () => {
+      it.todo('returns the default');
+    });
+
+    describe('when the default is not given', () => {
+      it.todo('throws');
     });
   });
 
-  describe('when there is env variable empty value', () => {
-    it('throws with ProcessEnvError', () => {
-      expect(() => envValidation({ AMQP_HOST: '' }))
-        .toThrow(ProcessEnvError);
+  describe('when the variable is required', () => {
+    describe('when the variable is found', () => {
+      it.todo('returns the variable');
+    });
+
+    describe('when the variable is not found', () => {
+      it.todo('throws');
     });
   });
 
-  describe('when there is no env variable missing value', () => {
-    it('does nothing', () => {
-      expect(() => envValidation({ AMQP_HOST: '127.0.0.1:5672' }))
-        .not.toThrow();
+  describe('when the type is unknow', () => {
+    it.todo('throws');
+  });
+
+  describe('when the type is not given', () => {
+    it.todo('throws');
+  });
+
+  describe('when the required and default are given', () => {
+    it.todo('throws');
+  });
+
+  // describe('when there are several types');
+
+  describe('when the default is given', () => {
+    describe('when the default is not the right type', () => {
+      it.todo('throws');
+    });
+
+    describe('when the default is the right type', () => {
+      it.todo('returns the variable');
+    });
+  });
+
+  describe('when variable is the right type', () => {
+    it('returns true', () => {
+      const CONTRACT = [{
+        variable: 'DB_HOST',
+        type: 'string',
+      }];
+      expect(env.validate(CONTRACT)).toBe(true);
+    });
+  });
+
+  describe('when variable is not the right type', () => {
+    it('throws', () => {
+      const CONTRACT = [{
+        variable: 'DB_HOST',
+        type: 'number',
+      }];
+
+      expect(() => env.validate(CONTRACT)).toThrow();
     });
   });
 });
