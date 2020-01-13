311848437c961e8c7a9ae915af80152e6d126829
FPierre <pflauder@gmail.com>
2020-01-13 22:00:14 +0100

    Add E2E tests

---
+++ src/index.spec.js
@@ -0,0 +1,27 @@
+const env = require('.');
+
+describe('src/index.js', () => {
+  beforeAll(() => {
+    process.env.API_URL = 'https://endpoint-a.pi/v3';
+    process.env.API_TOKEN = '0%f_a+cVF3';
+    process.env.DISABLED_USERS_PID = '321,987,654';
+  });
+
+  it('works', () => {
+    env.config([
+      {
+        key: 'API_URL',
+        type: 'url',
+        default: 'https://endpoint-a.pi/v1',
+      },
+      {
+        key: 'API_TOKEN',
+        type: 'string',
+        required: true,
+      },
+    ]);
+
+    expect(env.get('API_URL')).toBe('https://endpoint-a.pi/v3');
+    expect(env.get('API_TOKEN')).toBe(process.env.API_TOKEN);
+  });
+});
