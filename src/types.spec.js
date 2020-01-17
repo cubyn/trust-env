5adbe91434bac3cab55727bdea41ac84dc9840dc
FPierre <pflauder@gmail.com>
2020-01-17 15:25:45 +0100

    Add tests on stringsArray composed type

--- src/types.spec.js
+++ src/types.spec.js
@@ -2,6 +2,36 @@ require('./types');
 const isJs = require('is_js');
 
 describe('types', () => {
+  describe('stringsArray', () => {
+    it('exists', () => {
+      expect(isJs.stringsArray).toEqual(expect.any(Function));
+    });
+
+    it('supports native API', () => {
+      expect(isJs.not.stringsArray('abc')).toBe(true);
+      expect(isJs.not.stringsArray(['a'])).toBe(false);
+      expect(isJs.all.stringsArray([1], 'a')).toBe(false);
+      expect(isJs.all.stringsArray([['a'], ['b']])).toBe(true);
+      expect(isJs.any.stringsArray([['1', '2'], '1'])).toBe(true);
+      expect(isJs.any.stringsArray('1', 2)).toBe(false);
+    });
+
+    describe('when the payload is an array of strings', () => {
+      it('returns true', () => {
+        expect(isJs.stringsArray(['a'])).toBe(true);
+        expect(isJs.stringsArray(['a', 'b'])).toBe(true);
+      });
+    });
+
+    describe('when the payload is not an array of strings', () => {
+      it('returns false', () => {
+        expect(isJs.stringsArray(['a', [2]])).toBe(false);
+        expect(isJs.stringsArray([1, 2])).toBe(false);
+        expect(isJs.stringsArray(null)).toBe(false);
+      });
+    });
+  });
+
   describe('integersArray', () => {
     it('exists', () => {
       expect(isJs.integersArray).toEqual(expect.any(Function));
